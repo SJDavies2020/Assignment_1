7a50e8ae0eff31e7a6f86859d48399bd8d2a5093
SJDavies2020 <44582668+SJDavies2020@users.noreply.github.com>
2018-11-19 22:07:15 -0500

    Add files via upload
    Assignment #1

---
+++ Assignment_2/Form1.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace Assignment_2
+{
+    partial class frmCalcGrades
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtGrade1 = new System.Windows.Forms.TextBox();
+            this.txtGrade2 = new System.Windows.Forms.TextBox();
+            this.txtGrade3 = new System.Windows.Forms.TextBox();
+            this.txtGrade4 = new System.Windows.Forms.TextBox();
+            this.txtGrade5 = new System.Windows.Forms.TextBox();
+            this.txtGrade6 = new System.Windows.Forms.TextBox();
+            this.lblGrade1 = new System.Windows.Forms.Label();
+            this.lblGrade6 = new System.Windows.Forms.Label();
+            this.lblGrade5 = new System.Windows.Forms.Label();
+            this.lblGrade4 = new System.Windows.Forms.Label();
+            this.lblGrade3 = new System.Windows.Forms.Label();
+            this.lblGrade2 = new System.Windows.Forms.Label();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.lblOAGradeAvg = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(91, 258);
+            this.btnCalculate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(125, 48);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(39, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "COMP 1030";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(42, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "GNED 1005";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(39, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 24);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "COMP 1016";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(39, 139);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(120, 24);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "COMP 1053";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(39, 101);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(120, 24);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "COMP 1051";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(39, 63);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(120, 24);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "COMP 1002";
+            // 
+            // txtGrade1
+            // 
+            this.txtGrade1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGrade1.Location = new System.Drawing.Point(164, 18);
+            this.txtGrade1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtGrade1.Name = "txtGrade1";
+            this.txtGrade1.Size = new System.Drawing.Size(87, 30);
+            this.txtGrade1.TabIndex = 8;
+            // 
+            // txtGrade2
+            // 
+            this.txtGrade2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGrade2.Location = new System.Drawing.Point(164, 59);
+            this.txtGrade2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtGrade2.Name = "txtGrade2";
+            this.txtGrade2.Size = new System.Drawing.Size(87, 30);
+            this.txtGrade2.TabIndex = 9;
+            // 
+            // txtGrade3
+            // 
+            this.txtGrade3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGrade3.Location = new System.Drawing.Point(164, 97);
+            this.txtGrade3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtGrade3.Name = "txtGrade3";
+            this.txtGrade3.Size = new System.Drawing.Size(87, 30);
+            this.txtGrade3.TabIndex = 10;
+            // 
+            // txtGrade4
+            // 
+            this.txtGrade4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGrade4.Location = new System.Drawing.Point(164, 134);
+            this.txtGrade4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtGrade4.Name = "txtGrade4";
+            this.txtGrade4.Size = new System.Drawing.Size(87, 30);
+            this.txtGrade4.TabIndex = 11;
+            // 
+            // txtGrade5
+            // 
+            this.txtGrade5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGrade5.Location = new System.Drawing.Point(164, 172);
+            this.txtGrade5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtGrade5.Name = "txtGrade5";
+            this.txtGrade5.Size = new System.Drawing.Size(87, 30);
+            this.txtGrade5.TabIndex = 12;
+            // 
+            // txtGrade6
+            // 
+            this.txtGrade6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGrade6.Location = new System.Drawing.Point(164, 210);
+            this.txtGrade6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtGrade6.Name = "txtGrade6";
+            this.txtGrade6.Size = new System.Drawing.Size(87, 30);
+            this.txtGrade6.TabIndex = 13;
+            // 
+            // lblGrade1
+            // 
+            this.lblGrade1.AutoSize = true;
+            this.lblGrade1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGrade1.Location = new System.Drawing.Point(256, 23);
+            this.lblGrade1.Name = "lblGrade1";
+            this.lblGrade1.Size = new System.Drawing.Size(23, 24);
+            this.lblGrade1.TabIndex = 14;
+            this.lblGrade1.Text = "A";
+            // 
+            // lblGrade6
+            // 
+            this.lblGrade6.AutoSize = true;
+            this.lblGrade6.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGrade6.Location = new System.Drawing.Point(256, 215);
+            this.lblGrade6.Name = "lblGrade6";
+            this.lblGrade6.Size = new System.Drawing.Size(23, 24);
+            this.lblGrade6.TabIndex = 15;
+            this.lblGrade6.Text = "A";
+            // 
+            // lblGrade5
+            // 
+            this.lblGrade5.AutoSize = true;
+            this.lblGrade5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGrade5.Location = new System.Drawing.Point(256, 177);
+            this.lblGrade5.Name = "lblGrade5";
+            this.lblGrade5.Size = new System.Drawing.Size(23, 24);
+            this.lblGrade5.TabIndex = 16;
+            this.lblGrade5.Text = "A";
+            // 
+            // lblGrade4
+            // 
+            this.lblGrade4.AutoSize = true;
+            this.lblGrade4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGrade4.Location = new System.Drawing.Point(256, 139);
+            this.lblGrade4.Name = "lblGrade4";
+            this.lblGrade4.Size = new System.Drawing.Size(23, 24);
+            this.lblGrade4.TabIndex = 17;
+            this.lblGrade4.Text = "A";
+            // 
+            // lblGrade3
+            // 
+            this.lblGrade3.AutoSize = true;
+            this.lblGrade3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGrade3.Location = new System.Drawing.Point(256, 101);
+            this.lblGrade3.Name = "lblGrade3";
+            this.lblGrade3.Size = new System.Drawing.Size(23, 24);
+            this.lblGrade3.TabIndex = 18;
+            this.lblGrade3.Text = "A";
+            // 
+            // lblGrade2
+            // 
+            this.lblGrade2.AutoSize = true;
+            this.lblGrade2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGrade2.Location = new System.Drawing.Point(256, 63);
+            this.lblGrade2.Name = "lblGrade2";
+            this.lblGrade2.Size = new System.Drawing.Size(23, 24);
+            this.lblGrade2.TabIndex = 19;
+            this.lblGrade2.Text = "A";
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAverage.Location = new System.Drawing.Point(256, 322);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(23, 24);
+            this.lblAverage.TabIndex = 21;
+            this.lblAverage.Text = "A";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(39, 322);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(165, 24);
+            this.label14.TabIndex = 20;
+            this.label14.Text = "Overall Average:";
+            // 
+            // lblOAGradeAvg
+            // 
+            this.lblOAGradeAvg.AutoSize = true;
+            this.lblOAGradeAvg.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOAGradeAvg.Location = new System.Drawing.Point(256, 359);
+            this.lblOAGradeAvg.Name = "lblOAGradeAvg";
+            this.lblOAGradeAvg.Size = new System.Drawing.Size(23, 24);
+            this.lblOAGradeAvg.TabIndex = 23;
+            this.lblOAGradeAvg.Text = "A";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(50, 359);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(154, 24);
+            this.label8.TabIndex = 22;
+            this.label8.Text = "Overall Grade :";
+            // 
+            // frmCalcGrades
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(328, 394);
+            this.Controls.Add(this.lblOAGradeAvg);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.lblGrade2);
+            this.Controls.Add(this.lblGrade3);
+            this.Controls.Add(this.lblGrade4);
+            this.Controls.Add(this.lblGrade5);
+            this.Controls.Add(this.lblGrade6);
+            this.Controls.Add(this.lblGrade1);
+            this.Controls.Add(this.txtGrade6);
+            this.Controls.Add(this.txtGrade5);
+            this.Controls.Add(this.txtGrade4);
+            this.Controls.Add(this.txtGrade3);
+            this.Controls.Add(this.txtGrade2);
+            this.Controls.Add(this.txtGrade1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalculate);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.HelpButton = true;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "frmCalcGrades";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Grade Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtGrade1;
+        private System.Windows.Forms.TextBox txtGrade2;
+        private System.Windows.Forms.TextBox txtGrade3;
+        private System.Windows.Forms.TextBox txtGrade4;
+        private System.Windows.Forms.TextBox txtGrade5;
+        private System.Windows.Forms.TextBox txtGrade6;
+        private System.Windows.Forms.Label lblGrade1;
+        private System.Windows.Forms.Label lblGrade6;
+        private System.Windows.Forms.Label lblGrade5;
+        private System.Windows.Forms.Label lblGrade4;
+        private System.Windows.Forms.Label lblGrade3;
+        private System.Windows.Forms.Label lblGrade2;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label lblOAGradeAvg;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Assignment_2/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment_2
+{
+    // Assignment #1 
+    // Steven Davies - 200411361
+    // Grade Caculator
+
+    public partial class frmCalcGrades : Form
+    {
+        // Initalize Variables and set defaults
+
+        Label[] MyLabelArray = new Label[7];
+        TextBox[] MytxtBoxArray = new TextBox [6];
+        decimal TotalMarks =0;
+
+        public frmCalcGrades()
+        {
+            InitializeComponent();
+        }
+        #region // Form Load
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Initialize Label text property to be blank on load of form.
+            lblGrade1.Text = "";
+            lblGrade2.Text = "";
+            lblGrade3.Text = "";
+            lblGrade4.Text = "";
+            lblGrade5.Text = "";
+            lblGrade6.Text = "";
+            lblAverage.Text = "";
+            lblOAGradeAvg.Text = "";
+
+            // assign form control to the Label Control Array
+            MyLabelArray[0] = lblGrade1;
+            MyLabelArray[1] = lblGrade2;
+            MyLabelArray[2] = lblGrade3;
+            MyLabelArray[3] = lblGrade4;
+            MyLabelArray[4] = lblGrade5;
+            MyLabelArray[5] = lblGrade6;
+            MyLabelArray[6] = lblOAGradeAvg;
+
+            // assign form control to the Text Box Control Array
+            MytxtBoxArray[0] = txtGrade1;
+            MytxtBoxArray[1] = txtGrade2;
+            MytxtBoxArray[2] = txtGrade3;
+            MytxtBoxArray[3] = txtGrade4;
+            MytxtBoxArray[4] = txtGrade5;
+            MytxtBoxArray[5] = txtGrade6;
+        }
+        #endregion
+        #region // Grade Function
+        // Method to assign Grade Letter to Grade
+        public void RetGradeLeter(decimal num1, Label myLabel)
+        {
+            if (num1 > 80 && num1 < 101)
+            {
+                myLabel.Text = "A"; //Change label text to A
+                lblOAGradeAvg.Text = "A"; //Change label text to A
+            }
+            else if (num1 >70  && num1 < 79)
+            {
+                myLabel.Text = "B"; //Change label text to B
+                lblOAGradeAvg.Text = "B"; //Change label text to B
+            }
+            else if (num1 > 60 && num1 < 69) 
+            {
+                myLabel.Text = "C"; //Change label text to C
+                lblOAGradeAvg.Text = "C"; //Change label text to B
+            }
+            else if (num1 > 49 && num1 < 59)
+            {
+                myLabel.Text = "D"; //Change label text to D
+                lblOAGradeAvg.Text = "D"; //Change label text to B
+            }
+            
+            else
+            {
+                myLabel.Text = "FAIL";
+            }
+            TotalMarks = TotalMarks + num1;
+        }
+        #endregion
+        #region // Button Code 
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            // Loop through the control array and check textbox is not empty.
+            for (int a = 0; a <= 5; a++)
+            {
+               if (MytxtBoxArray[a].Text == "")
+                {
+                    MessageBox.Show("Some of your Values are Empty. Please Correct");
+                    return;
+                }
+            }
+            // Loop through the control array and check textbox is a number only.
+            TotalMarks = 0;
+            int txtCheck = 0;
+            for (int a = 0; a <= 5; a++) // Loop Settings
+            {
+                bool ValidateTxt; // True/False for test on next line
+                ValidateTxt = MytxtBoxArray[a].Text.All(char.IsDigit); // Check to see if the text is numeric
+                    if (ValidateTxt == false) // If not numeric then increment Number.
+                    {
+                        txtCheck = txtCheck +1;
+                    }
+             }
+            if (txtCheck > 0) // If checkvalue is not zero then show message
+            {
+                MessageBox.Show("Some of your input are not Numbers. Please Correct");
+                return;
+            }
+            // Loop through the control array and check textbox value is within range.
+            int ValCheck = 0;
+            for (int a = 0; a <= 5; a++) 
+            {
+                decimal CheckVal = System.Convert.ToDecimal(MytxtBoxArray[a].Text);
+                if (CheckVal < 0 | CheckVal > 100) // If checkvalue is not in range then increment counter.
+                {
+                    ValCheck = ValCheck +1;
+                }
+             }
+            if (ValCheck > 0) // If checkvalue is not zero then show message
+            {
+                MessageBox.Show("Some of your input is out of the Acceptable Range. Please Correct");
+                return;
+            }
+                // Loop through the control array, convert to decimal and cacl the average grade.
+                for (int a = 0; a <= 5; a++)
+                {
+                    RetGradeLeter(System.Convert.ToDecimal(MytxtBoxArray[a].Text), MyLabelArray[a]); // Loop and call Method to set grade letter.
+                }
+                lblAverage.Text = System.Convert.ToString(Math.Round((TotalMarks / 6),1)) + " %"; // Set Label TXT to set Grade Letter
+                RetGradeLeter(System.Convert.ToDecimal(Math.Round((TotalMarks / 6), 1)), MyLabelArray[6]); // Set final grade letter.
+        }
+        #endregion
+    }
+}
